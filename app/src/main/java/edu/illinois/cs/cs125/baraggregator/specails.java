d68bffe535a2edd71d06cbe745ce49e4c79603ea
Jacob <Langman2@illinois.edu>
2018-04-25 17:32:36 -0500

    Test

---
+++ app/src/main/java/edu/illinois/cs/cs125/baraggregator/specails.java
@@ -0,0 +1,17 @@
+package edu.illinois.cs.cs125.baraggregator;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+    public class specails extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.specails);
+        }
+}
